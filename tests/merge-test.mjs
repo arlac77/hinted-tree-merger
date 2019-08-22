c80fc344d402362d2c0a6959a172eb04501ddf17
Markus Felten <Markus.Felten@gmx.de>
2019-08-22 14:23:55 +0200

    test: scalar merges

--- tests/merge-test.mjs
+++ tests/merge-test.mjs
@@ -2,17 +2,32 @@ import test from "ava";
 import { merge } from "../src/merger.mjs";
 
 
-function macro(t, a, b, r) {
+function mt(t, a, b, r) {
   t.deepEqual(merge(a,b),r);
 }
 
-macro.title = (providedTitle = "merge", a, b) => `${providedTitle} ${a} ${b}`.trim();
+mt.title = (providedTitle = "merge", a, b) => `${providedTitle} ${a} ${b}`.trim();
 
-test(macro, 1, 2, 2);
-test(macro, 1, undefined, 1);
-//test(macro, undefined, 2, 2);
+test(mt, 1, 2, 2);
+test(mt, 1, undefined, 1);
+test(mt, undefined, 2, 2);
 
-test(macro, "a", "b", "b");
-//test(macro, "a", undefined, "a");
+test(mt, 11n, 22n, 22n);
+test(mt, 11n, undefined, 11n);
+test(mt, undefined, 22n, 22n);
 
-//test(macro, undefined, undefined, undefined);
+test(mt, "a", "b", "b");
+test(mt, "a", undefined, "a");
+test(mt, undefined, "b", "b");
+
+test(mt, true, false, false);
+test(mt, false, true, true);
+test(mt, true, undefined, true);
+test(mt, false, undefined, false);
+test(mt, undefined, false, false);
+test(mt, undefined, true, true);
+
+test(mt, undefined, undefined, undefined);
+test(mt, null, null, null);
+test(mt, undefined, null, null);
+test(mt, null, undefined, null);
