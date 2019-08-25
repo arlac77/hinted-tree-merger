9a37d901e1a9afaccd709da57aa7c2ed0a802e3a
Markus Felten <Markus.Felten@gmx.de>
2019-08-25 11:06:49 +0200

    test: use macro for isScalar

--- tests/scalar-test.mjs
+++ tests/scalar-test.mjs
@@ -1,18 +1,29 @@
 import test from "ava";
 import { isScalar } from "../src/util.mjs";
 
-test("isScalar", t => {
-  t.true(isScalar(1));
-  t.true(isScalar(1n));
-  t.true(isScalar("a"));
-  t.true(isScalar(true));
-  t.true(isScalar(false));
-  t.true(isScalar(undefined));
-  t.true(isScalar(null));
-  t.true(isScalar(Symbol()));
-  t.false(isScalar({}));
-  t.false(isScalar([]));
-  t.false(isScalar(new Date()));
-  t.false(isScalar(new Set()));
-  t.false(isScalar(new Map()));
-});
+function sc(t, a) {
+  t.true(isScalar(a));
+}
+
+sc.title = (providedTitle = "", a) =>
+  `isScalar ${providedTitle} ${a && a.description ? "Symbol" : a}`.trim();
+
+function nsc(t, a) {
+  t.false(isScalar(a));
+}
+nsc.title = (providedTitle = "", a) => `isScalar ${providedTitle} ${a}`.trim();
+
+test(sc, 1);
+test(sc, 2n);
+test(sc, "a");
+test(sc, true);
+test(sc, false);
+test(sc, undefined);
+test(sc, null);
+test(sc, Symbol("a symbol"));
+
+test(nsc, {});
+test(nsc, []);
+test(nsc, new Date());
+test(nsc, new Set());
+test(nsc, new Map());
