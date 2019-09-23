9f7375503d547e56bb7b36dee0e47777995a6042
Markus Felten <Markus.Felten@gmx.de>
2019-09-23 19:48:28 +0200

    test: preps for advances merging

---
+++ tests/merge-template-files-test.mjs
@@ -0,0 +1,60 @@
+import test from "ava";
+import { merge } from "../src/merger.mjs";
+
+test.skip("mergeTemplateFiles 2", t => {
+  t.deepEqual(
+    merge(
+      [
+        {
+          merger: "Package",
+          options: {
+            badges: [
+              {
+                name: "npm",
+                icon: "https://img.shields.io/npm/v/{{name}}.svg",
+                url: "https://www.npmjs.com/package/{{name}}"
+              }
+            ]
+          }
+        }
+      ],
+      [
+        {
+          merger: "Package",
+          options: {
+            badges: [
+              {
+                name: "npm1",
+                icon: "https://img.shields.io/npm/v/{{name}}.svg",
+                url: "https://www.npmjs.com/package/{{name}}"
+              }
+            ]
+          }
+        }
+      ],
+      {
+        '': { key: 'merger' },
+        'options.badges': { key: 'name' }
+      }
+    ),
+    [
+      {
+        merger: "Package",
+        options: {
+          badges: [
+            {
+              name: "npm",
+              icon: "https://img.shields.io/npm/v/{{name}}.svg",
+              url: "https://www.npmjs.com/package/{{name}}"
+            },
+            {
+              name: "npm1",
+              icon: "https://img.shields.io/npm/v/{{name}}.svg",
+              url: "https://www.npmjs.com/package/{{name}}"
+            }
+          ]
+        }
+      }
+    ]
+  );
+});
