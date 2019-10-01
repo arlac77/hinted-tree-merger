8ee6d67c1c3198403daaa0eeba4cd5ed15a40a5a
Markus Felten <Markus.Felten@gmx.de>
2019-10-01 23:33:07 +0200

    test: array cleanup

--- tests/merge-array-test.mjs
+++ tests/merge-array-test.mjs
@@ -1,24 +1,37 @@
 import test from "ava";
 import { mergeArrays } from "../src/merger.mjs";
 
-test("mergeArray", t => {
-  t.deepEqual(
-    mergeArrays(
-      [{ key: 1, value: 1 }, { key: 2, value: 2, something: 5 }],
-      [{ key: 2, value: 2, other: 4 }, { key: 3, value: 3 }],
-      "",
-      undefined,
-      {
-        "": {
-          key: "key",
-          sort: (a, b) => (a.key > b.key ? 1 : a.key < b.key ? -1 : 0)
-        }
-      }
-    ),
-    [
-      { key: 1, value: 1 },
-      { key: 2, value: 2, something: 5, other: 4 },
-      { key: 3, value: 3 }
-    ]
-  );
-});
+function mat(t, a, b, r, hints) {
+  let myActions = [];
+  t.deepEqual(mergeArrays(a, b, "", x => myActions.push(x), hints), r);
+}
+
+mat.title = (providedTitle = "merge", a, b) =>
+  `${providedTitle} ${JSON.stringify(a)} ${JSON.stringify(b)}`.trim();
+
+test(mat,
+  [{ key: 1, value: 1 }, { key: 2, value: 2, something: 5 }],
+  [{ key: 2, value: 2, other: 4 }, { key: 3, value: 3 }],
+  [
+    { key: 1, value: 1 },
+    { key: 2, value: 2, something: 5, other: 4 },
+    { key: 3, value: 3 }
+  ],  {
+    "*": {
+      key: "key",
+      sort: (a, b) => (a.key > b.key ? 1 : a.key < b.key ? -1 : 0)
+    }
+  }
+);
+
+test(mat,
+  ["a","b"],
+  ["-b"],
+  ["a"]
+);
+
+test(mat,
+  ["a","b"],
+  "-b",
+  ["a"]
+);
