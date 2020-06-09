deb290f2c3a7740772349e85677d9ecd8ebe1126
Markus Felten <Markus.Felten@gmx.de>
2020-06-09 13:15:04 +0200

    test: more on keyFor

--- tests/key-test.mjs
+++ tests/key-test.mjs
@@ -6,15 +6,21 @@ function kft(t, object, hint, expected) {
 }
 
 kft.title = (providedTitle = "", object, hint, expected) =>
-  `keyFor ${providedTitle} ${JSON.stringify(object)} ${JSON.stringify(hint)}`.trim();
-
+  `keyFor ${providedTitle} ${JSON.stringify(object)} ${JSON.stringify(
+    hint
+  )}`.trim();
 
 test(kft, {}, undefined, undefined);
 test(kft, { x: "v" }, { key: "k" }, undefined);
 test(kft, { k: "v" }, { key: "k" }, "v");
 
-test(kft, { k1: "v1", k2: "v2", k3: "v3" }, { key: ["k1","k2","k3"] }, "v1:v2:v3");
-test(kft, { k1: "v1", k3: "v3" }, { key: ["k1","k2","k3"] }, "v1::v3");
+test(
+  kft,
+  { k1: "v1", k2: "v2", k3: "v3" },
+  { key: ["k1", "k2", "k3"] },
+  "v1:v2:v3"
+);
+test(kft, { k1: "v1", k3: "v3" }, { key: ["k1", "k2", "k3"] }, "v1::v3");
 test(kft, { k1: "v1", k3: "v3" }, { key: ["k1"] }, "v1");
 
 test(kft, { k1: "v1", k2: "v2", k3: "v3" }, { key: "k1&k2&k3" }, "v1:v2:v3");
@@ -23,3 +29,18 @@ test(kft, { k1: "v1", k3: "v3" }, { key: "k1&k2&k3" }, "v1::v3");
 test(kft, { k1: "v1", k2: "v2", k3: "v3" }, { key: "k1|k2|k3" }, "v1");
 test(kft, { k2: "v2", k3: "v3" }, { key: "k1|k2|k3" }, "v2");
 
+test(
+  kft,
+  {
+    uses: "gr2m/create-or-update-pull-request-action@v1.x",
+    env: "GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}",
+    with: {
+      path: "README.md",
+      title: "Sync API into README",
+      branch: "readme-api",
+      "commit-message": "docs(README): sync API"
+    }
+  },
+  { key: "id|name|uses|run" },
+  "gr2m/create-or-update-pull-request-action@v1.x"
+);
