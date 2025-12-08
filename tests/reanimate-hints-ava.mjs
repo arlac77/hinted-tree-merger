39ef03177a52132bd03abbafb685d0a0466b1fc0
Markus Felten <Markus.Felten@gmx.de>
2025-12-08 16:20:15 +0000

    test: reanimateHints with regexp orderBy

--- tests/reanimate-hints-ava.mjs
+++ tests/reanimate-hints-ava.mjs
@@ -1,8 +1,17 @@
 import test from "ava";
 import { reanimateHints, mergeVersions } from "hinted-tree-merger";
 
-test("reanimateHints", t => {
+test("reanimateHints merge", t => {
   t.deepEqual(reanimateHints({ a: { merge: "mergeVersions" } }), {
     a: { merge: mergeVersions }
   });
+});
+
+test("reanimateHints orderBy regexp", t => {
+  t.deepEqual(
+    reanimateHints({ a: { orderBy: ["a", "/[A-Z]+$/i", "/\\d+/", 1] } }),
+    {
+      a: { orderBy: ["a", new RegExp(/[A-Z]+$/, "i"), new RegExp(/\d+/), 1] }
+    }
+  );
 });
