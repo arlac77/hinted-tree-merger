443c2e60392e58bbcd9034663c3c1c1ee86bfacd
Markus Felten <Markus.Felten@gmx.de>
2025-12-08 16:19:52 +0000

    feat: reanimte Regexp in orderBy hints

--- src/reanimate-hints.mjs
+++ src/reanimate-hints.mjs
@@ -19,13 +19,25 @@ const mergeFunctions = [
 
 export function reanimateHints(hints) {
   for (const { value, path, parents } of walk(hints)) {
-    if (path[path.length - 1] === "merge") {
-      for (const f of mergeFunctions) {
-        if (f.name === value) {
-          parents[parents.length - 1].merge = f;
-          break;
+    switch (path[path.length - 1]) {
+      case "merge":
+        for (const f of mergeFunctions) {
+          if (f.name === value) {
+            parents[parents.length - 1].merge = f;
+            break;
+          }
         }
-      }
+        break;
+      case "orderBy":
+        parents[parents.length - 1].orderBy = value.map(v => {
+          if (typeof v === "string" && v[0] === "/" && v.match(/\/[img]?$/)) {
+            const m = v.match(/\/([a-z]*)$/)
+            const inner = v.substring(1,v.length - m[0].length)
+            return new RegExp(inner,m[1]);
+          }
+          return v;
+        });
+        break;
     }
   }
 
